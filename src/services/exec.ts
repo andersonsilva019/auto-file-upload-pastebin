22d989360de8f4d67cb7ff8e8de4e21590d3628c
Andreazo Silva Souza <andreazocoisas@gmail.com>
2022-05-29 16:02:34 -0300

    added notepad executable

--- src/services/exec.ts
+++ src/services/exec.ts
@@ -1,20 +1,24 @@
 import { access } from "fs/promises";
 import { constants } from "fs";
 import { promisify } from "util";
+import { resolve } from "path";
 import { exec as execChildProcess } from "child_process";
 import { platform } from "os";
 
 const exec = promisify(execChildProcess);
 
 async function openEditorToFile(pathToFile: string) {
+  const pathToEditor = resolve(
+    __dirname,
+    "..",
+    "lib",
+    "executable",
+    "notepad",
+    "notepad++.exe"
+  );
   await access(`${pathToFile}`, constants.W_OK | constants.R_OK);
 
-  const defaultEditorFrom = {
-    linux: "kate",
-    win32: "notepad",
-  };
-  const os = platform();
-  await exec(`${defaultEditorFrom[os]} ${pathToFile}`);
+  await exec(`${pathToEditor} ${pathToFile}`);
 }
 
 export { openEditorToFile };
